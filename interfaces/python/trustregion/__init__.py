3a20379e278c2816d4b140fb41d7a14e3be681cc
Lindon Roberts <lindon.roberts@gmail.com>
2026-09-10 17:40:52 +1000

    First attempt at Python interface

---
+++ interfaces/python/trustregion/__init__.py
@@ -0,0 +1,5 @@
+
+__version__ = '1.0'
+
+from .interface import trsunc, arcunc, trsapp, trsbox, trslin
+__all__ = ['trsunc', 'trsapp', 'trsbox', 'trslin']
---
+++ interfaces/python/trustregion/interface.py
@@ -0,0 +1,2 @@
+import numpy as np
+from ._trs import trsunc, arcunc, trsapp, trsbox, trslin
